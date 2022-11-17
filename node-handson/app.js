c3bcf8a2a16492ff2f1ac78a16d80b0694e5eec9
khaliliali <ali.khalili@outlook.com>
2022-11-17 21:14:53 +0800

    Async Pattern.

--- node-handson/app.js
+++ node-handson/app.js
@@ -1,2 +1,15 @@
-console.log('test app')
+const http = require("http");
 
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Home page");
+  } else if (req.url === "/about") {
+    res.end("About page");
+  } else {
+    res.end("error page");
+  }
+});
+
+server.listen(3131, () => {
+  console.log("Server is Running... ! ");
+});
