13ce807477ba4f3d7a3aa0ebcf31834cb4172762
khaliliali <ali.khalili@outlook.com>
2022-11-22 17:13:49 +0800

    Add GetALL, GetById and Delete Task fuctions

--- node-handson/03-task-manager/final/app.js
+++ node-handson/03-task-manager/final/app.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
-const express = require("express");
+const express = require('express');
 const app = express();
-const tasks = require("./routes/tasks");
-const connectDB = require("./db/connect");
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
 
 // Middleware
 app.use(express.json());
 
 // Routes
-app.use("/api/v1/tasks", tasks);
+app.use('/api/v1/tasks', tasks);
 
 // Ports
 const port = 3000;
--- node-handson/03-task-manager/final/controller/tasks.js
+++ node-handson/03-task-manager/final/controller/tasks.js
@@ -1,23 +1,57 @@
-const Task = require("./../models/task");
+const Task = require('../models/task');
 
-const getAllTasks = (req, res) => {
-  res.send("All tasks");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks: tasks });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
-const createTask = (req, res) => {
-  //   res.send("Create a task");
-  res.json(req.body);
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
-const getTasks = (req, res) => {
-  //   res.send("Get single task");
-  res.json({ id: req.params.id });
+const getTasks = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const taskById = await Task.findOne({ _id: taskID });
+
+    if (!taskById) {
+      return res.status(404).json({ msg: `This task is not exist: ${taskID}` });
+    }
+
+    res.status(200).json({ taskById });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
+
 const updateTasks = (req, res) => {
-  res.send("Update tasks");
+  res.send('Update tasks');
 };
-const deleteTasks = (req, res) => {
-  res.send("Delete tasks");
+
+const deleteTasks = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const taskById = await Task.findOneAndDelete({ _id: taskID });
+
+    if (!taskById) {
+      return res
+        .status(404)
+        .json({ msg: `This task is already deleted : ${taskID}` });
+    }
+
+    res.status(200).json({ taskById });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
 module.exports = {
